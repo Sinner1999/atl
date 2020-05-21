6fad552f947a4cfffaffba02a3f23aa868007905
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-21 13:39:35 +0300

    123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    A T L
                 </div>
 
                 <div class="links">
