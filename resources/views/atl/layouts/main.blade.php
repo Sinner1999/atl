f28bd4d90b0918758b448b5dc04a92b452f1e0aa
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-21 17:39:50 +0300

    123

--- resources/views/atl/layouts/main.blade.php
+++ resources/views/atl/layouts/main.blade.php
@@ -999,8 +999,8 @@
 </div>
 
 <!-- Javascript -->
-<script src="assets/bundles/libscripts.bundle.js"></script>    
-<script src="assets/bundles/vendorscripts.bundle.js"></script>
-<script src="assets/bundles/mainscripts.bundle.js"></script>
+<script src="{{ asset('assets/bundles/libscripts.bundle.js') }}"></script>    
+<script src="{{ asset('assets/bundles/vendorscripts.bundle.js') }}"></script>
+<script src="{{ asset('assets/bundles/mainscripts.bundle.js') }}"></script>
 </body>
 </html>
