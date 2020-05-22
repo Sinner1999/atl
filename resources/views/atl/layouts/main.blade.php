3db198c357fb95973bc3c1475d93212823da6035
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-22 15:57:28 +0300

    123

--- resources/views/atl/layouts/main.blade.php
+++ resources/views/atl/layouts/main.blade.php
@@ -153,13 +153,13 @@
             <div class="container-fluid">
 
                 <div class="navbar-left">
-                    <div class="navbar-btn">
+                    <!-- <div class="navbar-btn">
                         <a href="index.html"><img src="{{ asset('assets/images/icon.svg') }}"
                                 alt="Oculux Logo" class="img-fluid logo"></a>
                         <button type="button" class="btn-toggle-offcanvas"><i
                                 class="lnr lnr-menu fa fa-bars"></i></button>
-                    </div>
-                    <ul class="nav navbar-nav">
+                    </div> -->
+                    <!-- <ul class="nav navbar-nav">
                         <li class="dropdown">
                             <a href="javascript:void(0);" class="dropdown-toggle icon-menu" data-toggle="dropdown">
                                 <i class="icon-envelope"></i>
@@ -299,7 +299,7 @@
                         </li>
                         <li class="p_news"><a href="page-news.html" class="news icon-menu" title="News">News</a></li>
                         <li class="p_blog"><a href="page-blog.html" class="blog icon-menu" title="Blog">Blog</a></li>
-                    </ul>
+                    </ul> -->
                 </div>
 
                 <div class="navbar-right">
