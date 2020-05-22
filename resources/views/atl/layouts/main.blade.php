67bf43175860b88a341abeca32aa3bdb3f48a53b
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-22 13:29:55 +0300

    123

--- resources/views/atl/layouts/main.blade.php
+++ resources/views/atl/layouts/main.blade.php
@@ -928,7 +928,7 @@
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
-                                    <i class="icon-power"></i>         /////////////////////////////
+                                    <i class="icon-power"></i>
                                     Logout
                                 </a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
