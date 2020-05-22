1fde5bb621a72c26bf74b4d23eb42e84a3580fee
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-22 17:31:33 +0300

    123

--- resources/views/atl/layouts/main.blade.php
+++ resources/views/atl/layouts/main.blade.php
@@ -1120,7 +1120,7 @@
                             </div>
                             <div class="body">
                                 <div class="table-responsive">
-                                    <table class="table table-hover js-basic-example dataTable table-primary">
+                                    <table class="table table-hover js-basic-example dataTable table-info">
                                         <thead>
                                             <tr>
                                                 <th><strong>Фамилия Имя</strong></th>
