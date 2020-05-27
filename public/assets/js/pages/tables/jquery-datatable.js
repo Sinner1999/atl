1601d1e76567ceeae4287b0cf4e5e5a6ad5ded48
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-27 14:58:14 +0300

    123

--- public/assets/js/pages/tables/jquery-datatable.js
+++ public/assets/js/pages/tables/jquery-datatable.js
@@ -4,9 +4,9 @@ $(function () {
     // Exportable table
     $('.js-exportable').DataTable({
         dom: 'Bfrtip',
-        // buttons: [
-        //     'copy', 'csv', 'excel', 'pdf', 'print'
-        // ]
+        buttons: [
+
+        ]
     });
 });
 
