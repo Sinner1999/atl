da2fa83a689d4b1d4622660ed014321bb3194d33
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-27 16:50:57 +0300

    123

--- resources/views/atl/phone.blade.php
+++ resources/views/atl/phone.blade.php
@@ -13,18 +13,7 @@
                 <div class="card">
                     <div class="header">
                         <h2>Basic Table <small>Basic example without any additional modification classes</small></h2>
-                        {{-- <ul class="header-dropdown dropdown">
 
-                            <li><a href="javascript:void(0);" class="full-screen"><i class="icon-frame"></i></a></li>
-                            <li class="dropdown">
-                                <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="javascript:void(0);">Action</a></li>
-                                    <li><a href="javascript:void(0);">Another Action</a></li>
-                                    <li><a href="javascript:void(0);">Something else</a></li>
-                                </ul>
-                            </li>
-                        </ul> --}}
                     </div>
                     <div class="body">
                         <div class="table-responsive">
