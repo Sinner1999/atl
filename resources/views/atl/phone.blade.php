425f4e47ebb445fbb4afc80f7c1c42989a66211c
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-28 15:34:23 +0300

    123

--- resources/views/atl/phone.blade.php
+++ resources/views/atl/phone.blade.php
@@ -31,6 +31,7 @@
                                         <td>Бухгалтерия</td>
 
                                     </tr> --}}
+                                    @foreach ($users as $usr)
                                     <tr>
                                         <td></td>
                                         <td>
@@ -38,130 +39,24 @@
                                                 <div class="dropdown">
 
                                                     <a href="javascript:void(0);" class="dropdown-toggle user-name"
-                                                        data-toggle="dropdown">Гаврилко Сергей</a>
+                                                        data-toggle="dropdown">{{ $usr->name}}</a>
                                                     <ul class="dropdown-menu dropdown-menu-right account vivify flipInY">
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>25150</li>
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>+375-44-537-11-11</li>
-                                                        <li><i class="icon-envelope" style="margin: 10px;"></i>sergey_gavrilko@atlantm.com</li>
+                                                        @foreach($usr->phones as $ph)
+                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>{{ $ph->num}}</li>
+                                                        @endforeach
+                                                        <li><i class="icon-envelope" style="margin: 10px;"></i>{{ $usr->email }}</li>
                                                     </ul>
                                                 </div>
                                             </div>
                                         </td>
                                         <td>25150</td>
-                                        <td>Директор</td>
-                                        <td>Администрация</td>
+                                        <td>{{ $usr->posit->name }}</td>
+                                        <td>{{ $usr->posit->dept->name }}</td>
 
                                     </tr>
+                                    @endforeach
 
-                                    <tr>
-                                        <td></td>
-                                        <td>
-                                            <div class="sidebar-scroll">
-                                                <div class="dropdown">
 
-                                                    <a href="javascript:void(0);" class="dropdown-toggle user-name"
-                                                        data-toggle="dropdown">Крепская Александра</a>
-                                                    <ul class="dropdown-menu dropdown-menu-right account vivify flipInY">
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>25157</li>
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>+375-29-378-36-04</li>
-                                                        <li><i class="icon-envelope" style="margin: 10px;"></i>aleksandra_krepskaya@atlantm.com</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>25157</td>
-                                        <td>Финансовый менеджер</td>
-                                        <td>Финансовый отдел</td>
-
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td>
-                                            <div class="sidebar-scroll">
-                                                <div class="dropdown">
-
-                                                    <a href="javascript:void(0);" class="dropdown-toggle user-name"
-                                                        data-toggle="dropdown">Засеко Елена</a>
-                                                    <ul class="dropdown-menu dropdown-menu-right account vivify flipInY">
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>25156</li>
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>+375-29-692-02-01</li>
-                                                        <li><i class="icon-envelope" style="margin: 10px;"></i>elena_zaseko@atlantm.com</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>25156</td>
-                                        <td>Главный бухгалтер</td>
-                                        <td>Бухгалтерия</td>
-
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td>
-                                            <div class="sidebar-scroll">
-                                                <div class="dropdown">
-
-                                                    <a href="javascript:void(0);" class="dropdown-toggle user-name"
-                                                        data-toggle="dropdown">Минец Юлия</a>
-                                                    <ul class="dropdown-menu dropdown-menu-right account vivify flipInY">
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>25153</li>
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>+375-29-668-54-65</li>
-                                                        <li><i class="icon-envelope" style="margin: 10px;"></i>yulia_minets@atlantm.com</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>25153</td>
-                                        <td>Руководитель отдела маркетинга</td>
-                                        <td>Отдел маркеринга</td>
-
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td>
-                                            <div class="sidebar-scroll">
-                                                <div class="dropdown">
-
-                                                    <a href="javascript:void(0);" class="dropdown-toggle user-name"
-                                                        data-toggle="dropdown">Крек Сергей</a>
-                                                    <ul class="dropdown-menu dropdown-menu-right account vivify flipInY">
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>25100</li>
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>26100</li>
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>+375-29-837-12-97</li>
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>+375-29-620-75-37</li>
-                                                        <li class="divider"></li>
-                                                        <li><i class="icon-envelope" style="margin: 10px;"></i>sergey_krek@atlantm.com</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>25100</td>
-                                        <td>Руководитель отдела продаж автомобилей</td>
-                                        <td>Отдел продаж автомобилейя</td>
-
-                                    </tr>
-
-                                    <tr>
-                                        <td></td>
-                                        <td>
-                                            <div class="sidebar-scroll">
-                                                <div class="dropdown">
-
-                                                    <a href="javascript:void(0);" class="dropdown-toggle user-name"
-                                                        data-toggle="dropdown">Таубалдина Жанна</a>
-                                                    <ul class="dropdown-menu dropdown-menu-right account vivify flipInY">
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>25140</li>
-                                                        <li><i class="icon-call-end" style="margin: 10px;"></i>+375-29-750-59-16</li>
-                                                        <li><i class="icon-envelope" style="margin: 10px;"></i>zhanna_taubaldina@atlantm.com</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>25140</td>
-                                        <td>Руководитель отдела по работе с клиентами</td>
-                                        <td>Отдел по работе с клиентами</td>
-
-                                    </tr>
 
                                 </tbody>
                             </table>
