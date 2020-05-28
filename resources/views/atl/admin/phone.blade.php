6c103c6960f8a961bd87ec7dbd6cf79d3bb55b63
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-05-28 11:30:35 +0300

    123

--- resources/views/atl/admin/phone.blade.php
+++ resources/views/atl/admin/phone.blade.php
@@ -16,7 +16,7 @@
                     </div>
                     <div class="body">
                         <div class="table-responsive">
-                            <table class="table table-striped bg-blue table-hover dataTable js-exportable-admin">
+                            <table class="table table-striped bg-blue text-light table-hover dataTable js-exportable-admin">
                                 <thead>
                                     <tr>
                                         <th><strong>Фамилия Имя</strong></th>
