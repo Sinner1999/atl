e75e99834af346789616a4fda60ce6691c104268
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-06-02 14:58:03 +0300

    123

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends Controller
         return view('atl.home', $data);
     }
 
-    public function ozchroot()
+    public function ozch()
     {
         // $users = Empl::all();
 
