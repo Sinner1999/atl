d5031bee57efd50d82cc5f44bd88fc4c84b3f0d5
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-06-03 17:12:50 +0300

    123

--- resources/views/atl/layouts/menu.blade.php
+++ resources/views/atl/layouts/menu.blade.php
@@ -40,9 +40,9 @@
                 <li class="header">Меню</li>
                 <li><a href="/"><i class="icon-speedometer"></i><span>Справочник</span></a></li>
                 <li><a href="/doc"><i class="icon-doc"></i><span>Документы</span></a></li>
-                @if (Auth::user()->name == 'ozch')
-                <li><a href="/ozch"><i class="icon-badge"></i><span>ОЗЧ</span></a></li>
-                @endif
+                {{-- @if (Auth::user()->name == 'ozch') --}}
+            <li><a href="/ozch"><i class="icon-badge"></i><span>ОЗЧ {{ Auth::user() }}</span></a></li>
+                {{-- @endif --}}
                 @auth
                     <li class="header">Администратор</li>
                     <li><a href="/admin"><i class="icon-users"></i><span>Сотрудники</span></a></li>
