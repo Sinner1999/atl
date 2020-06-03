bbb6eb2768852b71542b6c6958d119ea0019088e
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-06-03 17:04:25 +0300

    123

--- resources/views/atl/layouts/menu.blade.php
+++ resources/views/atl/layouts/menu.blade.php
@@ -16,7 +16,7 @@
                 <a href="javascript:void(0);" class="dropdown-toggle user-name"
                     data-toggle="dropdown"><strong>Louis Pierce</strong></a>
                 <ul class="dropdown-menu dropdown-menu-right account vivify flipInY">
-                    @auth
+                    @auth('admin')
                         <li>
                             <a href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
@@ -40,8 +40,10 @@
                 <li class="header">Меню</li>
                 <li><a href="/"><i class="icon-speedometer"></i><span>Справочник</span></a></li>
                 <li><a href="/doc"><i class="icon-doc"></i><span>Документы</span></a></li>
+                @auth('ozch')
                 <li><a href="/ozch"><i class="icon-badge"></i><span>ОЗЧ</span></a></li>
-                @auth
+                @endauth
+                @auth('admin')
                     <li class="header">Администратор</li>
                     <li><a href="/admin"><i class="icon-users"></i><span>Сотрудники</span></a></li>
                     <li><a href="/admin/dept"><i class="icon-briefcase"></i><span>Отделы</span></a></li>
