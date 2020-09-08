9764847bafb78f4b10e605a3b6667a6f2e58208a
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-09-08 14:16:05 +0300

    123

--- resources/views/atl/ozch/home.blade.php
+++ resources/views/atl/ozch/home.blade.php
@@ -17,7 +17,7 @@
                     <div class="body">
                         <div class="table-responsive">
 
-                            {{ $au }}
+                            {{ int($au) }}
 
                         </div>
 
