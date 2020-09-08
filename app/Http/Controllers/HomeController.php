fddd4005296233b6f0d7a852ffd761cac79ec44a
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-09-08 14:57:12 +0300

    123

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -82,12 +82,30 @@ class HomeController extends Controller
 
     public function ozch()
     {
+
+        $headers = array(
+            'cache-control: max-age=0',
+            'upgrade-insecure-requests: 1',
+            'user-agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+            'sec-fetch-user: ?1',
+            'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'x-compress: null',
+            'sec-fetch-site: none',
+            'sec-fetch-mode: navigate',
+            'accept-encoding: deflate, br',
+            'accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        );
+
+
         $ch = curl_init('https://parts.toyota-motor.ru/rest/jofl.doHelo.aws');
         // curl_setopt($ch, CURLOPT_URL, 'https://parts.toyota-motor.ru/rest/jofl.doHelo.aws');
-        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, __DIR__ . '/cookie.txt');
+        curl_setopt($ch, CURLOPT_COOKIEJAR, __DIR__ . '/cookie.txt');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        $data = curl_exec($ch);
+        $dt = curl_exec($ch);
         // $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         // var_dump($http_code );
 
@@ -95,7 +113,7 @@ class HomeController extends Controller
 
         $data = [
             'content' => 'atl.ozch.home',
-            'au' => $data,
+            'au' => $dt,
             // 'users' => $users
         ];
         return view('atl.home', $data);
