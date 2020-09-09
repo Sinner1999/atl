5d8667de1450d37a169f556cab0a12db4d8a1935
Рудый Евгений Олегович <Eorudyi@minsk.by.dom>
2020-09-09 09:02:15 +0300

    123

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -124,7 +124,7 @@ class HomeController extends Controller
 
         $data = [
             'content' => 'atl.ozch.home',
-            'au' => $dt,
+            // 'au' => $dt,
             'st' => $cookies,
             // 'users' => $users
         ];
